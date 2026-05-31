1c3b8f5399bc2e86f0da8306a8c9c69f31aac3ef
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2026-05-31 21:12:01 +0700

    update credential

--- tests/test_positive.py
+++ tests/test_positive.py
@@ -4,6 +4,13 @@ from pages import login
 from pages.login import Login
 from pages.products import Products
 from tests.conftest import setup
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+username = os.getenv("TEST_USERNAME")
+password = os.getenv("TEST_PASSWORD")
 
 
 @allure.title('Login Positif')
@@ -13,8 +20,8 @@ def test_login_positif(setup):
     
         login = Login(setup)
 
-        login.input_username('standard_user')
-        login.input_password('secret_sauce')
+        login.input_username(username)
+        login.input_password(password)
         login.click_login_button()
 
         Products(setup).check_url_products('https://www.saucedemo.com/inventory.html')
